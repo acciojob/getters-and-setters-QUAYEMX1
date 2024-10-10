453f6ca79e061ad355f4f1ebd6b0725b88d97a52
QUAYEMX1 <126396459+QUAYEMX1@users.noreply.github.com>
2024-10-10 13:17:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name; // use _name to follow convention for private-like variables
+        this._age = age;
+    }
+    
+    // Getter for name
+    get name() {
+        return this._name;
+    }
 
-class Student extends Person {}
+    // Setter for age
+    set age(newAge) {
+        this._age = newAge;
+    }
 
-class Teacher extends Person {}
+    // Getter for age (optional if needed)
+    get age() {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
